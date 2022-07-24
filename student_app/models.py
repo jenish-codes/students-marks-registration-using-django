c900f4bc2d03c78868c0b85fec5545b9f0c3ed85
jenish-codes <jenish.codes@gmail.com>
2022-07-24 10:43:40 +0530

    initial migration

--- student_app/models.py
+++ student_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class StudentDetails(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.CharField(max_length=100)
+    tamil = models.IntegerField(default=0)
+    english = models.IntegerField(default=0)
+    maths = models.IntegerField(default=0)
+    science = models.IntegerField(default=0)
+    social = models.IntegerField(default=0)
+    total = models.IntegerField(default=0)
+    pass_fail = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
